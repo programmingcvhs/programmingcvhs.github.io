71479734b90ced9b027c7aab6dae28be78a29742
programmingcvhs <arisenatmosphere@gmail.com>
2025-08-21 20:43:44 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  output: 'export',
+  trailingSlash: true,
   images: {
-    domains: ['api.placeholder.com'],
+    unoptimized: true,
   },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/pawd-club' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/pawd-club' : '',
 }
 
 module.exports = nextConfig
