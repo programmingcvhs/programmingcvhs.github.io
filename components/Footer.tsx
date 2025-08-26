0fec2a747fd331be518c673deb57f4b33da609a2
programmingcvhs <arisenatmosphere@gmail.com>
2025-08-25 21:18:16 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,7 @@ const footerLinks = {
   ],
   social: [
     { href: 'https://github.com/programmingcvhs', icon: Github, label: 'GitHub' },
-    { href: 'https://instagram.com/programmingcvhs', icon: Instagram, label: 'Instagram' },
+    { href: 'https://instagram.com/programming_cvhs', icon: Instagram, label: 'Instagram' },
     { href: 'mailto:s1831922@online.houstonisd.org', icon: Mail, label: 'Email' }
   ]
 }
